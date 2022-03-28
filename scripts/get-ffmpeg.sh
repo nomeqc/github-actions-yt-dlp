4032aa786d5c18f553b115b2b59a4433545805f6
Nomeqc <nomeqc@gmail.com>
2022-03-28 12:41:42 +0800

    Update get-ffmpeg.sh

--- scripts/get-ffmpeg.sh
+++ scripts/get-ffmpeg.sh
@@ -1,2 +1,3 @@
-curl -L https://github.com/Nomeqc/my-release/releases/download/ffmpeg/ffmpeg -o ./bin/ffmpeg
-curl -L https://github.com/Nomeqc/my-release/releases/download/ffmpeg/ffprobe -o ./bin/ffprobe
+mkdir -p bin
+curl -L https://github.com/Nomeqc/my-release/releases/download/ffmpeg/ffmpeg -o bin/ffmpeg
+curl -L https://github.com/Nomeqc/my-release/releases/download/ffmpeg/ffprobe -o bin/ffprobe
