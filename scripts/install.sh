b8a413321b66d33ba69008404408df0927d87846
Nomeqc <nomeqc@gmail.com>
2022-03-28 12:45:06 +0800

    Update and rename get-ffmpeg.sh to install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,7 @@
 mkdir -p bin
 curl -L https://github.com/Nomeqc/my-release/releases/download/ffmpeg/ffmpeg -o bin/ffmpeg
 curl -L https://github.com/Nomeqc/my-release/releases/download/ffmpeg/ffprobe -o bin/ffprobe
+chmod a+rx bin/ffmpeg
+chmod a+rx bin/ffprobe
+# 将bin目录添加进PATH
+echo "$(cd ./bin; pwd)" >> $GITHUB_PATH
