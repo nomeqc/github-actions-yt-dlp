9479b1975799a8f28d50146dddbc35625734e171
Nomeqc <nomeqc@gmail.com>
2022-03-28 13:45:38 +0800

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,5 +3,4 @@ curl -L https://github.com/Nomeqc/my-release/releases/download/ffmpeg/ffmpeg -o 
 curl -L https://github.com/Nomeqc/my-release/releases/download/ffmpeg/ffprobe -o $TOOLS_PATH/ffprobe
 chmod a+rx $TOOLS_PATH/ffmpeg
 chmod a+rx $TOOLS_PATH/ffprobe
-# 将bin目录添加进PATH
-echo "$(cd $TOOLS_PATH; pwd)" >> $GITHUB_PATH
+
