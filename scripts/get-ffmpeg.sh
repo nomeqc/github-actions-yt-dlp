5431a3861142bdea478fd9bf2773496376495f9d
Nomeqc <nomeqc@gmail.com>
2022-03-28 12:40:55 +0800

    Create get-ffmpeg.sh

---
+++ scripts/get-ffmpeg.sh
@@ -0,0 +1,2 @@
+curl -L https://github.com/Nomeqc/my-release/releases/download/ffmpeg/ffmpeg -o ./bin/ffmpeg
+curl -L https://github.com/Nomeqc/my-release/releases/download/ffmpeg/ffprobe -o ./bin/ffprobe
