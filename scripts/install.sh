574b193f53de176ad80ca224c5ef1b38efcf4936
Fallrainy <nomeqc@gmail.com>
2022-03-29 13:58:48 +0800

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,11 +5,11 @@ echo "$(cd $TOOLS_PATH; pwd)" >> $GITHUB_PATH
 if [[ -f requirements.txt ]]; then pip install -r requirements.txt; fi
 # 下载ffmpeg
 if [[ "$TOOLS_CACHE_HIT" != "true" ]]; then
-    curl -L https://github.com/Nomeqc/my-release/releases/download/ffmpeg/ffmpeg -o $TOOLS_PATH/ffmpeg
-    curl -L https://github.com/Nomeqc/my-release/releases/download/ffmpeg/ffprobe -o $TOOLS_PATH/ffprobe
+    curl -Lfo $TOOLS_PATH/ffmpeg https://github.com/Nomeqc/my-release/releases/download/ffmpeg/ffmpeg
+    curl -Lfo $TOOLS_PATH/ffprobe https://github.com/Nomeqc/my-release/releases/download/ffmpeg/ffprobe
     chmod a+rx $TOOLS_PATH/ffmpeg
     chmod a+rx $TOOLS_PATH/ffprobe
 fi
 # 下载yt-dlp
-curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
+curl -Lfo /usr/local/bin/yt-dlp https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp
 chmod a+rx /usr/local/bin/yt-dlp
