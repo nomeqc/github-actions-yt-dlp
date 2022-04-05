e514756ad5d2ecd4f5a2dce10af40b179a3cef33
Fallrainy <nomeqc@gmail.com>
2022-04-05 08:09:00 +0800

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,22 +1,27 @@
+down_file(){
+    url=$1
+    filename=$2
+    echo "下载：$url"
+    curl -# -Lfo "$filename" "$url"
+    echo ""
+    chmod a+rx "$filename"
+}
+
 mkdir -p "$TOOLS_PATH"
+
+script_dir="$(cd $(dirname "${BASH_SOURCE[0]}") || exit; pwd)"
+
 # 将tools目录添加进PATH
 echo "$(cd "$TOOLS_PATH" || exit; pwd)" >> $GITHUB_PATH
-# 安装python依赖
-if [[ -f requirements.txt ]]; then pip install -r requirements.txt; fi
 
-ffmpeg_url="https://github.com/Nomeqc/my-release/releases/download/ffmpeg/ffmpeg"
-ffprobe_url="https://github.com/Nomeqc/my-release/releases/download/ffmpeg/ffprobe"
-ytdlp_url="https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp"
+# 安装python依赖
+if [[ -f $script_dir/requirements.txt ]]; then pip install -r $script_dir/requirements.txt; fi
+
 # 下载ffmpeg
 if [[ "$TOOLS_CACHE_HIT" != "true" ]]; then
-    echo "下载：$ffmpeg_url"
-    curl -# -Lfo "$TOOLS_PATH/ffmpeg" "$ffmpeg_url"
-    echo "下载：$ffprobe_url"
-    curl -# -Lfo "$TOOLS_PATH/ffprobe" "$ffprobe_url"
-    chmod a+rx "$TOOLS_PATH/ffmpeg"
-    chmod a+rx "$TOOLS_PATH/ffprobe"
+    down_file "https://github.com/Nomeqc/my-release/releases/download/ffmpeg/ffmpeg" "$TOOLS_PATH/ffmpeg"
+    down_file "https://github.com/Nomeqc/my-release/releases/download/ffmpeg/ffprobe" "$TOOLS_PATH/ffprobe"
 fi
+
 # 下载yt-dlp
-echo "下载：$ytdlp_url"
-curl -# -Lfo /usr/local/bin/yt-dlp "$ytdlp_url"
-chmod a+rx /usr/local/bin/yt-dlp
+down_file "https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp" "/usr/local/bin/yt-dlp"
